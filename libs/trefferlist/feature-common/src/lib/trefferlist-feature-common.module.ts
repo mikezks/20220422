56e78ed6fddc8b2c0a2a59bdc43d239b9f9c9125
mikezks <23002661+mikezks@users.noreply.github.com>
2022-05-31 17:37:41 +0200

    feat: trefferlist features

--- libs/trefferlist/feature-common/src/lib/trefferlist-feature-common.module.ts
+++ libs/trefferlist/feature-common/src/lib/trefferlist-feature-common.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrefferlistDomainModule } from '@projected-routes/trefferlist/domain';
 import { CommonComponent } from './common.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [CommonModule, TrefferlistDomainModule],
-  declarations: [CommonComponent],
+  declarations: [CommonComponent, AboutComponent, HomeComponent],
   exports: [CommonComponent],
 })
 export class TrefferlistFeatureCommonModule {}
