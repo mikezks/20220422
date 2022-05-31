c1c7cc50bcf599212309eeb33a888207a657772b
mikezks <23002661+mikezks@users.noreply.github.com>
2022-05-31 17:36:20 +0200

    feat: recherche features

--- libs/recherche/feature-common/src/lib/recherche-feature-common.module.ts
+++ libs/recherche/feature-common/src/lib/recherche-feature-common.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RechercheDomainModule } from '@projected-routes/recherche/domain';
 import { CommonComponent } from './common.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports: [CommonModule, RechercheDomainModule],
-  declarations: [CommonComponent],
+  declarations: [CommonComponent, HomeComponent, AboutComponent],
   exports: [CommonComponent],
 })
 export class RechercheFeatureCommonModule {}
